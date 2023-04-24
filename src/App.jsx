d0470fcd80594507e3ee26908b63bfcef379ae30
Xinpu <liuxinpu16@gmail.com>
2023-04-23 20:06:40 -0700

    v1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from "./features/counter/Counter";
+import "./app.css";
+
+function App() {
+  return (
+    <>
+      <div>app</div>
+      <Counter />
+    </>
+  );
+}
+
+export default App;
---
+++ src/features/counter/Counter.jsx
@@ -0,0 +1,37 @@
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, reset, increaseByAmount } from "./counterSlice";
+import { useState } from "react";
+
+const Counter = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+  const [amount, setAmount] = useState(0);
+  const addValue = Number(amount) || 0;
+  const resetAll = () => {
+    setAmount(0);
+    dispatch(reset());
+  };
+
+  return (
+    <div>
+      <p>{count}</p>
+      <div>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+      <div>
+        <button onClick={resetAll}>reset</button>
+        <button onClick={() => dispatch(increaseByAmount(addValue))}>
+          increase By Amount
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
